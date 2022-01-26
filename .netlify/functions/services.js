4ac00c35d905ec84a6cef79f6a78465410d59f47
daniel <danieljkhoo@gmail.com>
2022-01-26 17:49:09 +0800

    fix: manually set gas fees

--- .netlify/functions/services.js
+++ .netlify/functions/services.js
@@ -5,16 +5,17 @@ const _ = require('lodash');
 const bluebird = require('bluebird')
 const ipfsAPI = require("ipfs-http-client");
 const ipfs = ipfsAPI({ host: "ipfs.infura.io", port: "5001", protocol: "https" });
-
-
 // Provider to connect to local blockchain
 const localProviderUrl = "http://localhost:8545";
 // Provider to connect via RPCUrl
-const rpcProviderUrl = "https://rpc-mainnet.maticvigil.com/";
+// https://rpc-mainnet.maticvigil.com/
+const rpcProviderUrl = "https://polygon-mainnet.g.alchemy.com/v2/dLLMLJx3t3JroncLDlaU7vRceYS4mOcG";
+// const rpcProviderUrl = "https://rpc-mumbai.maticvigil.com"
+// const rpcProviderUrl = "https://rinkeby.infura.io/v3/0381915c632140b78efa49ff3f94ac0c";
 
 // Set config with ENVs
 const providerUrl = process.env.PROD === 'true' ? rpcProviderUrl : localProviderUrl;
-const provider = new ethers.providers.StaticJsonRpcProvider(providerUrl);
+const provider = new ethers.providers.JsonRpcProvider(providerUrl);
 
 const deployedContractAddress = process.env.CONTRACT_ADDRESS
 const hotWalletAddress = process.env.HOT_WALLET_ADDRESS
@@ -26,19 +27,24 @@ const sendToAddress = async (recipientAddress, tokenId) => {
     // Contract abi isn't providing autocomplete but can reference docs for ERC-721 NFTS
     // https://docs.openzeppelin.com/contracts/3.x/api/token/erc721
     const contract = new ethers.Contract(deployedContractAddress, abi, signer);
-
     // Check owner is hot wallet i.e. it hasnt been redeemed
     const owner = await contract.ownerOf(tokenId)
     console.log(`tokenId: ${tokenId} owner: ${owner}`);
     if (owner !== hotWalletAddress) throw new Error('TokenId has already been redeemed')
 
     // Transfer to recipient
-    // await contract.transferFrom(signer.address, recipientAddress, tokenId)
+    const txn = await contract.transferFrom(signer.address, recipientAddress, tokenId, { gasLimit: 285000, gasPrice: ethers.utils.parseUnits('500', 'gwei') });
+    console.log(txn);
+    console.log(txn.maxFeePerGas.toNumber());
+    console.log(txn.maxPriorityFeePerGas.toNumber());
 
+    // const est = await contract.estimateGas.transferFrom(signer.address, recipientAddress, tokenId)
+    // console.log(est.toNumber());
     return recipientAddress
 }
 
 const getTokensForAddress = async (address) => {
+    console.log(address, deployedContractAddress, providerUrl);
     // Contract abi isn't providing autocomplete but can reference docs for ERC-721 NFTS
     // https://docs.openzeppelin.com/contracts/3.x/api/token/erc721
     const contract = new ethers.Contract(deployedContractAddress, abi, signer);
